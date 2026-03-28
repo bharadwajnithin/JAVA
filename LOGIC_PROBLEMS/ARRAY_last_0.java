2ccf3965da4a8161b53ebe4d4a6bb0fb8bd869b9
bharadwajnithin <bharadwajnithin666@gmail.com>
2026-03-28 23:11:26 +0530

    Added ARRAY_last_0 logic

---
+++ LOGIC_PROBLEMS/ARRAY_last_0.java
@@ -0,0 +1,32 @@
+package LOGIC_PROBLEMS;
+import java.util.*;
+
+public class ARRAY_last_0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		
+		int arr[]=new int[4];
+		System.out.print("enter  the arr of :");
+		
+		for(int i=0;i<=arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+		
+		
+		
+		
+		for (int i= 0;i<arr.length;i++) {
+			if(arr[i]==0) {
+				
+				
+				
+			}
+		}
+		
+
+	}
+
+}
