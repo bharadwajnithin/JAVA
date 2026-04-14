55ded8c2f31ce0b73bd8708c107d342d7c8b04a4
bharadwajnithin <bharadwajnithin666@gmail.com>
2026-04-14 16:38:24 +0530

    file handling

--- FileHandling/readerex2.java
+++ FileHandling/readerex2.java
@@ -1,10 +1,25 @@
 package FileHandling;
+import java.io.*;
 
 public class readerex2 {
 
 	public static void main(String[] args) {
+		try {
 		// TODO Auto-generated method stub
-
+		BufferedReader br =new BufferedReader(new FileReader("D:\\Engee\\Codex DNY\\JAVA  WORKSPACE FOR PERSONAL\\Learning\\src\\FileHandling\\HI.txt") );
+		
+		
+		String line;
+		while((line=br.readLine())!= null) {
+			System.out.println(line);
+		}
+		
+		}
+		catch(Exception e) {
+			
+			System.out.println(e);
+		}
+		
 	}
 
 }
